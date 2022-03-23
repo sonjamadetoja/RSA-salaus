9cc1c9088c06769f079f2df10c3b8e59826c6c3c
Sonja Madetoja <sonja.madetoja@helsinki.fi>
2022-03-23 15:42:28 +0200

    fix test setUp

--- src/tests/keycreationmachine_test.py
+++ src/tests/keycreationmachine_test.py
@@ -1,12 +1,12 @@
 import unittest
-from keycreationmachine import KeyCreationMachine as KCM
+from keycreationmachine import KeyCreationMachine
 
 class TestKeyCreationMachine(unittest.TestCase):
     def setUp(self):
-        pass
+        self.KCM = KeyCreationMachine()
 
     def test_generate_random_bits_length_correct(self):
-        random_bit_number = KCM.generate_random_bits(1024)
+        random_bit_number = self.KCM.generate_random_bits(1024)
         l = len(bin(random_bit_number))-2
 
         self.assertEqual(l, 1024)
