92fffc81d7259f122035c26f8822362f551419e7
Sonja Madetoja <sonja.madetoja@helsinki.fi>
2022-03-24 10:16:28 +0200

    add docstring

--- src/keycreationmachine.py
+++ src/keycreationmachine.py
@@ -1,14 +1,30 @@
 import random
 
 class KeyCreationMachine:
+    """Tämä luokka sisältää funktiot, joita tarvitaan salausavainparin luomiseen.
+    """
     def __init__(self):
-        pass
+        """Tämä on luokan konstruktori.
+        """
 
     def generate_random_bits(self, length):
+        """Tämä funktio luo halutun pituisen satunnaisluvun.
+
+        Args:
+            length (int): Haluttu bittien määrä.
+
+        Returns:
+            int: satunnaisluku
+        """
         return random.randrange(2**(length-1)+1, 2**length-1)
 
     def miller_rabin_test(self, candidate, number_of_rounds):
-        pass
+        """Tämä funktio tarkistaa, onko annettu luku alkuluku.
+
+        Args:
+            candidate (int): Tarkistettava luku.
+            number_of_rounds (int): Testikierrosten määrä
+        """
 
     # def generate_key(self):
     #     rand_bits = self.generate_random_bits(1024)
