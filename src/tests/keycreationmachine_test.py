0ff0c519ab2c3187907530fd47fb291556898fdd
Sonja Madetoja <sonja.madetoja@helsinki.fi>
2022-03-31 11:01:57 +0300

    add tests for checking and generating prime

--- src/tests/keycreationmachine_test.py
+++ src/tests/keycreationmachine_test.py
@@ -10,3 +10,30 @@ class TestKeyCreationMachine(unittest.TestCase):
         l = len(bin(random_bit_number))-2
 
         self.assertEqual(l, 1024)
+
+    def test_check_if_prime_true(self):
+        result = self.KCM.check_if_prime(203956878356401977405765866929034577280193993314348263094772646453283062722701277632936616063144088173312372882677123879538709400158306567338328279154499698366071906766440037074217117805690872792848149112022286332144876183376326512083574821647933992961249917319836219304274280243803104015000563790123, 40)
+
+        self.assertEqual(result, True)
+
+    def test_check_if_prime_false(self):
+        result = self.KCM.check_if_prime(203956878356401977405765866929034577280193993314348263094772646453283062722701277632936616063144088173312372882677123879538709400158306567338328279154499698366071906766440037074217117805690872792848149112022286332144876183376326512083574821647933992961249917319836219304274280243803104015000563790121, 40)
+
+        self.assertEqual(result, False)
+
+    def test_check_if_prime_less_than_three(self):
+        result = self.KCM.check_if_prime(1, 1)
+
+        self.assertEqual(result, True)
+
+    def test_check_if_prime_divisible_by_two(self):
+        result = self.KCM.check_if_prime(4, 1)
+
+        self.assertEqual(result, False)
+
+    def test_generate_prime(self):
+        prime = self.KCM.generate_prime(1024)
+        length_of_p = len(bin(prime))-2
+
+        self.assertEqual(type(prime), int)
+        self.assertEqual(length_of_p, 1024)
